4ff5e782a5132b88dac9b70f9c785f6032e00d8e
vdugeri <verem.dugeri@gmail.com>
2019-10-02 16:06:48 +0100

    Change flex to grid display

--- src/components/post-preview/post-preview.component.jsx
+++ src/components/post-preview/post-preview.component.jsx
@@ -5,19 +5,17 @@ import "./post-preview.styles.scss";
 const PostPreview = ({ post }) => (
   <div className="post-preview">
     <div className="post-preview__metadata">
-      <img src={post.author.image} alt="author" />
+      <div className="post-preview--author-image">
+        {post.author.username.substring(0, 1).toUpperCase()}
+      </div>
       <div className="post-preview__metadata--right">
         <div className="post-preview__author">{post.author.username}</div>
         <div className="post-preview__date">Oct 2, 2019</div>
       </div>
     </div>
     <div className="post-preview__details">
-      <h2 className="post-preview__topic">Eyes can't focus</h2>
-      <p className="post-preview__summary">
-        My eyes are sensitive to light but they are especially sensitive at
-        night even when I’m at home? It’s really hard for my eyes to focus on
-        anything and I don’t know why because I wear glasses. Why …
-      </p>
+      <h2 className="post-preview__topic">{post.title}</h2>
+      <p className="post-preview__summary">{post.body}</p>
       <span>Be the first to reply</span>
     </div>
   </div>
--- src/pages/forum/category.data.js
+++ src/pages/forum/category.data.js
@@ -6,19 +6,115 @@ export default [
   {
     name: "General Health",
     posts: [
-      { author: { image: healthIcon, username: "verem" } },
-      { author: { image: healthIcon, username: "verem" } }
+      {
+        author: {
+          image: healthIcon,
+          username: "verem"
+        },
+        body: `My eyes are sensitive to light but they are especially sensitive at
+        night even when I’m at home? It’s really hard for my eyes to focus on
+				anything and I don’t know why because I wear glasses. Why …`,
+        title: `Eyes can't focus`
+      },
+      {
+        author: {
+          image: healthIcon,
+          username: "Kenis"
+        },
+        body: `My eyes are sensitive to light but they are especially sensitive at
+        night even when I’m at home? It’s really hard for my eyes to focus on
+				anything and I don’t know why because I wear glasses. Why …`,
+        title: `Eyes can't focus`
+      },
+      {
+        author: {
+          image: healthIcon,
+          username: "David"
+        },
+        body: `My eyes are sensitive to light but they are especially sensitive at
+        night even when I’m at home? It’s really hard for my eyes to focus on
+				anything and I don’t know why because I wear glasses. Why …`,
+        title: `Eyes can't focus`
+      },
+      {
+        author: {
+          image: healthIcon,
+          username: "Ken"
+        },
+        body: `My eyes are sensitive to light but they are especially sensitive at
+        night even when I’m at home? It’s really hard for my eyes to focus on
+				anything and I don’t know why because I wear glasses. Why …`,
+        title: `Eyes can't focus`
+      }
     ],
     icon: healthIcon
   },
   {
     name: "Politics",
     icon: politicsIcon,
-    posts: [{ author: { image: healthIcon, username: "verem" } }]
+    posts: [
+      {
+        author: { image: healthIcon, username: "Dan" },
+        body: `My eyes are sensitive to light but they are especially sensitive at
+        night even when I’m at home? It’s really hard for my eyes to focus on
+				anything and I don’t know why because I wear glasses. Why …`,
+        title: `Eyes can't focus`
+      },
+      {
+        author: { image: healthIcon, username: "Mnena" },
+        body: `My eyes are sensitive to light but they are especially sensitive at
+        night even when I’m at home? It’s really hard for my eyes to focus on
+				anything and I don’t know why because I wear glasses. Why …`,
+        title: `Eyes can't focus`
+      },
+      {
+        author: { image: healthIcon, username: "Mercy" },
+        body: `My eyes are sensitive to light but they are especially sensitive at
+        night even when I’m at home? It’s really hard for my eyes to focus on
+				anything and I don’t know why because I wear glasses. Why …`,
+        title: `Eyes can't focus`
+      },
+      {
+        author: { image: healthIcon, username: "Nguamo" },
+        body: `My eyes are sensitive to light but they are especially sensitive at
+        night even when I’m at home? It’s really hard for my eyes to focus on
+				anything and I don’t know why because I wear glasses. Why …`,
+        title: `Eyes can't focus`
+      }
+    ]
   },
   {
     name: "Economy",
     icon: economyIcon,
-    posts: [{ author: { image: healthIcon, username: "verem" } }]
+    posts: [
+      {
+        author: { image: healthIcon, username: "Debby" },
+        body: `My eyes are sensitive to light but they are especially sensitive at
+        night even when I’m at home? It’s really hard for my eyes to focus on
+				anything and I don’t know why because I wear glasses. Why …`,
+        title: `Eyes can't focus`
+      },
+      {
+        author: { image: healthIcon, username: "Alex" },
+        body: `My eyes are sensitive to light but they are especially sensitive at
+        night even when I’m at home? It’s really hard for my eyes to focus on
+				anything and I don’t know why because I wear glasses. Why …`,
+        title: `Eyes can't focus`
+      },
+      {
+        author: { image: healthIcon, username: "Bendozy" },
+        body: `My eyes are sensitive to light but they are especially sensitive at
+        night even when I’m at home? It’s really hard for my eyes to focus on
+				anything and I don’t know why because I wear glasses. Why …`,
+        title: `Eyes can't focus`
+      },
+      {
+        author: { image: healthIcon, username: "DaroTobi" },
+        body: `My eyes are sensitive to light but they are especially sensitive at
+        night even when I’m at home? It’s really hard for my eyes to focus on
+				anything and I don’t know why because I wear glasses. Why …`,
+        title: `Eyes can't focus`
+      }
+    ]
   }
 ];
