75361bb6ffaca0c6b6d7ac03bfc23fa44ee620d6
vdugeri <verem.dugeri@gmail.com>
2019-10-03 16:53:50 +0100

    Make radio button a reusable component

---
+++ src/components/custom-radio-button/custom-radio-button.component.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import "./custom-radio-button.styles.scss";
+
+const CustomRadioButton = ({ handleChange, label, name, ...otherProps }) => (
+  <div className="radio-group">
+    <input
+      type="radio"
+      name={name}
+      className="radio-group__radio-input"
+      id={label}
+      onChange={handleChange}
+      {...otherProps}
+    />
+    <label htmlFor={label} className="radio-group__radio-label">
+      <div className="radio-group__radio-button"></div>
+      {label}
+    </label>
+  </div>
+);
+
+export default CustomRadioButton;
--- src/components/topic-select/topic-select.component.jsx
+++ src/components/topic-select/topic-select.component.jsx
@@ -1,26 +1,18 @@
 import React from "react";
-import { connect } from "react-redux";
-import { createStructureSelector } from "reselect";
+
+import CustomRadioButton from "../custom-radio-button/custom-radio-button.component";
 
 import "./topic-select.styles.scss";
 
 const TopicSelect = ({ topic, handleChange }) => {
   return (
     <div className="topic-select">
-      <div className="topic-select__radio-group">
-        <input
-          type="radio"
-          name="post-topic"
-          className="topic-select__radio-input"
-          id={topic.id}
-          onChange={handleChange}
-          value={topic.id}
-        />
-        <label htmlFor={topic.id} className="topic-select__radio-label">
-          <div className="topic-select__radio-button"></div>
-          {topic.name}
-        </label>
-      </div>
+      <CustomRadioButton
+        label={topic.name}
+        name="topic"
+        handleChange={handleChange}
+        value={topic.id}
+      />
     </div>
   );
 };
