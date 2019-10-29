de55d9f1779c22ed8cdddbec6451d5b24199fbb7
vdugeri <verem.dugeri@gmail.com>
2019-10-29 13:44:24 +0100

    Refactor

---
+++ src/components/loader/loader.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./loader.styles.scss";
+
+const Loader = () => (
+  <div className="loader-overlay">
+    <div className="loader-container"></div>
+  </div>
+);
+
+export default Loader;
--- src/pages/user-posts/user-posts.component.jsx
+++ src/pages/user-posts/user-posts.component.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useCallback } from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
